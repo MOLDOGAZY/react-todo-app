73cc737a9f5dd82abef5c59d551c30f910eab05e
MOLDOGAZY <moldogazy.kabylbekov.kk@gmail.com>
2022-02-05 20:27:05 +0600

    completed todo app

---
+++ src/components/footer.component.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer-task">
+      <span>Drag and drop to reorder list</span>
+      <div className="attribution">
+        Coded by
+        <a href="https://github.com/MOLDOGAZY" target="_blank">
+          MOLDOGAZY
+        </a>
+        .
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
